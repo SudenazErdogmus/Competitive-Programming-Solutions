c3e9110b9e5948d07d8e128f983e32a60621c02f
Sudenaz Erdoğmuş <133746647+SudenazErdogmus@users.noreply.github.com>
2026-01-10 21:19:39 +0300

    Add solution for Weather Observation Station 1

---
+++ HackerRank/SQL/Basic-Select/weather-observation-station-1.sql
@@ -0,0 +1,15 @@
+/*
+PROBLEM: Weather Observation Station 1
+PLATFORM: HackerRank
+DIFFICULTY: Easy
+LANGUAGE: MS SQL SERVER (T-SQL)
+LINK: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+
+PROBLEM DESCRIPTION:
+Query a list of CITY and STATE from the STATION table.
+
+SOLUTION APPROACH:
+1. Used SELECT to retrieve CITY and STATE columns from STATION table as requested.
+*/
+
+SELECT CITY, STATE FROM STATION;
