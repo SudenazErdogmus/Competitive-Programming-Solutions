782f43228f6e1618645802e2ac1b6efd4fda8804
Sudenaz Erdoğmuş <133746647+SudenazErdogmus@users.noreply.github.com>
2026-01-10 20:53:08 +0300

    Ad solution for Select All

---
+++ HackerRank/SQL/Basic-Select/select-all.sql
@@ -0,0 +1,15 @@
+/*
+PROBLEM: Select All
+PLATFORM: HackerRank
+DIFFICULTY: Easy
+LANGUAGE: MS SQL SERVER (T-SQL)
+LINK:https://www.hackerrank.com/challenges/select-all-sql/problem
+
+PROBLEM DESCRIPTION:
+Query all columns (attributes) for every row in the CITY table.
+
+SOLUTION APPROACH:
+1. Used SELECT * to retrieve all columns as requested.
+*/
+
+SELECT * FROM CITY;
