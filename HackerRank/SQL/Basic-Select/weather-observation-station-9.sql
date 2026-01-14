f185223dc5d5f2a2ba1ce3e5bcf117ab1c8e86a4
Sudenaz Erdoğmuş <133746647+SudenazErdogmus@users.noreply.github.com>
2026-01-14 23:59:30 +0300

    Update and rename weather-observation-station.sql to weather-observation-station-9.sql

---
+++ HackerRank/SQL/Basic-Select/weather-observation-station-9.sql
@@ -0,0 +1,15 @@
+/*
+PROBLEM: Weather Observation Station 9
+PLATFORM: HackerRank
+DIFFICULTY: Easy
+LANGUAGE: MS SQL SERVER (T-SQL)
+LINK: https://www.hackerrank.com/challenges/weather-observation-station-9/problem
+
+PROBLEM DESCRIPTION:
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+SOLUTION APPROACH:
+
+*/
+
+SELECT DISTINCT CITY FROM STATION
